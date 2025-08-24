126653d84218ebf66cb5d0a8e864580ad9a25124
Pen Menghong <penmenghong7@gmail.com>
2025-08-24 14:55:36 +0700

    Updates

---
+++ assets/table-CwVd8_Fv.js
@@ -0,0 +1,11 @@
+import{c as l}from"./createLucideIcon-Cydmb8Mh.js";import{j as t,a as s}from"./index-CR5JE19F.js";/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6",key:"4alrt4"}],["path",{d:"M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2",key:"v07s0e"}],["line",{x1:"10",x2:"10",y1:"11",y2:"17",key:"1uufr5"}],["line",{x1:"14",x2:"14",y1:"11",y2:"17",key:"xtxkd"}]],d=l("trash-2",o);/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M11.5 15H7a4 4 0 0 0-4 4v2",key:"15lzij"}],["path",{d:"M21.378 16.626a1 1 0 0 0-3.004-3.004l-4.01 4.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z",key:"1817ys"}],["circle",{cx:"10",cy:"7",r:"4",key:"e45bow"}]],i=l("user-pen",r);function b({className:e,...a}){return t.jsx("div",{"data-slot":"table-container",className:"relative w-full overflow-x-auto",children:t.jsx("table",{"data-slot":"table",className:s("w-full caption-bottom text-sm",e),...a})})}function x({className:e,...a}){return t.jsx("thead",{"data-slot":"table-header",className:s("[&_tr]:border-b",e),...a})}function h({className:e,...a}){return t.jsx("tbody",{"data-slot":"table-body",className:s("[&_tr:last-child]:border-0",e),...a})}function u({className:e,...a}){return t.jsx("tr",{"data-slot":"table-row",className:s("hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",e),...a})}function m({className:e,...a}){return t.jsx("th",{"data-slot":"table-head",className:s("text-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",e),...a})}function p({className:e,...a}){return t.jsx("td",{"data-slot":"table-cell",className:s("p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",e),...a})}export{b as T,i as U,x as a,u as b,m as c,h as d,p as e,d as f};
